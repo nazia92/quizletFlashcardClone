4feb96b99c73957f1a8d595451736f049f976718
nazia92 <nazia.rangwala@gmail.com>
2025-10-31 03:18:06 -0400

    finished quizlet

--- script.js
+++ script.js
@@ -12,10 +12,89 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+  
+    if(showingTerm == true){
+        getContents.innerText = flashcards[currentIndex].term
+
+    }
+
+    else{
+        getContents.innerText = flashcards[currentIndex].definition
+    }
 
 }
 
 // The rest of the code you will write is apart of event listeners
 
-// This line will display the card when the page is refreshed
+//switching between def and term
+let getContents = document.getElementById("card-content")
+getContents.addEventListener("click", () => {
+    if(showingTerm){
+        showingTerm = false
+         console.log(`hi`)
+    }
+    else{
+        showingTerm = true
+         console.log(`hello`)
+    }
+
+    displayCard() //recalling the method
+    
+  
+    
+
+})
+
+//nextButton
+let nextButton = document.getElementById("next-btn")
+nextButton.addEventListener("click", () => {
+    if(currentIndex == flashcards.length-1){
+        currentIndex = 0
+    }
+
+    else {
+        currentIndex++
+    }
+
+    displayCard() //recalling the method
+    
+})
+
+//prevButton
+let prevButton = document.getElementById("prev-btn")
+prevButton.addEventListener("click", () => {
+    if(currentIndex == 0){
+        currentIndex = flashcards.length-1
+    }
+    else{
+        currentIndex--
+    }
+
+    displayCard() //recalling the method
+})
+
+//adding terms
+let inputTerm = document.getElementById("new-term")
+let inputDef = document.getElementById("new-definition")
+
+
+
+let addButton = document.getElementById("add-card-btn")
+addButton.addEventListener("click", () => {
+    let newTerm = inputTerm.value
+    let newDef = inputDef.value  
+    flashcards.push({term: newTerm, definition: newDef });
+    currentIndex = flashcards.length-1
+    inputTerm.value = "";
+    inputDef.value = "";
+
+
+    displayCard() //recalling the method
+})
+
+
+
+ 
 window.onload = displayCard;
+ 
+
